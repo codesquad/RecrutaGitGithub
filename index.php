f19938b897d332dccf92666d12467d3fc77777ff
Elton Minetto <eminetto@code-squad.com>
2013-02-13 20:08:35 +0000

    Novo método

--- index.php
+++ index.php
@@ -7,5 +7,9 @@ $app->get('/hello/{name}', function($name) use($app) {
     return 'Hello '.$app->escape($name); 
 }); 
 
+$app->get('/', function() use($app) {
+	return 'OK';
+});
+
 //executa
 $app->run();
