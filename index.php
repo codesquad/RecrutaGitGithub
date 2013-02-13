6aea5153d9c89561992ea05120ee0c96bc3dfabc
Elton Minetto <eminetto@gmail.com>
2013-02-13 17:55:46 -0200

    Removido comentário

--- index.php
+++ index.php
@@ -3,7 +3,6 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application(); 
 
-//essa linha mostra o hello world
 $app->get('/hello/{name}', function($name) use($app) { 
     return 'Hello '.$app->escape($name); 
 }); 
